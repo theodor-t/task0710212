603c111f4649427b95b57e906718dfd9a60685bf
theodorerlax <sampjohn@mail.ru>
2021-10-07 08:42:26 +0300

    create model Product

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,24 @@
+package model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private String price;
+
+    @Column(name = "weight")
+    private String weight;
+}
